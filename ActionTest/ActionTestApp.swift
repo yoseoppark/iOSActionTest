790e9264b2578ad2251703800725c1c8b1216181
김두현 <doohyeonkim@hyundai.com>
2021-07-06 09:07:46 +0900

    Initial Commit

---
+++ ActionTest/ActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionTestApp.swift
+//  ActionTest
+//
+//  Created by doohyeon on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct ActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActionTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActionTest
+//
+//  Created by doohyeon on 2021/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
